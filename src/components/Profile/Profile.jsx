b011efc37525807c534466e410d52ad7a58946aa
InnaIvBoiko <inna_boiko@libero.it>
2024-03-31 11:34:53 +0200

    Task 1.v2

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,33 +1,45 @@
-export const Profile = () => {
+import css from './Profile.module.css';
+
+export default function Profile({
+    userData: {
+        username,
+        tag,
+        location,
+        avatar,
+        stats: {
+            followers, 
+            views,
+            likes,    
+        },
+    }
+}) {
     return (
-        <div className="profile-wrap">
+        <div className={css.container}>
             <div>
                 <img
-                    className="profile-img"
-                    src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
+                    className={css.avatar}
+                    src={avatar}
                     alt="User avatar"
                 />
-                <p className="profile-title">Petra Marica</p>
-                <p>@pmarica</p>
-                <p>Salvador, Brasil</p>
+                <p className={css.title}>{username}</p>
+                <p className={css.text}>@{tag}</p>
+                <p className={css.text}>{location}</p>
             </div>
 
-            <ul className="profile-list">
-                <li className="profile-list-item">
+            <ul className={css.list}>
+                <li className={css.stat}>
                     <span>Followers</span>
-                    <span>1000</span>
+                    <span className={css.bold}>{followers}</span>
                 </li>
-                <li className="profile-list-item">
+                <li className={css.stat}>
                     <span>Views</span>
-                    <span>2000</span>
+                    <span className={css.bold}>{views}</span>
                 </li>
-                <li className="profile-list-item">
+                <li className={css.stat}>
                     <span>Likes</span>
-                    <span>3000</span>
+                    <span className={css.bold}>{likes}</span>
                 </li>
             </ul>
         </div>
     );
-};
-
-export default Profile;
+}
